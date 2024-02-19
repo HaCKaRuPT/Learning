7fa90c6009aae9e82695792750a1463a9941cdc3
HaCKaRu <44715202+HaCKaRuPT@users.noreply.github.com>
2024-02-19 11:30:43 +0000

    ex_12_upd

--- jscript/aula_ex_02/index.js
+++ jscript/aula_ex_02/index.js
@@ -9,19 +9,21 @@ const carregar = () =>{
     var data = new Date()
     var hora = data.getHours()
 
+    var hora = 23
+
     msg.innerHTML = `Agora são ${hora} horas!`
 
     if (hora >= 0 && hora < 12) {
         //bom dia
-        foto.src = "manha-cropped.png"
+        foto.src = "/aula_ex_02/images/manha-cropped.png"
         document.body.style.background = '#e2cd9f'
     } else if (hora >=12 && hora < 18){
         // boa tarde
-        foto.src = "tarde-cropped.png"
+        foto.src = "/aula_ex_02/images/tarde-cropped.png"
         document.body.style.background = '#b9846f'
     }else{
         // Boa noite
-        foto.src = "noite-cropped.png"
+        foto.src = "/aula_ex_02/images/noite-cropped.png"
         document.body.style.background = '#515154'
     }
 }
