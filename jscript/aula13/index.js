ea51ae9ef532ca09cc0ea4d753d1ca9258a29ed1
HaCKaRu <44715202+HaCKaRuPT@users.noreply.github.com>
2024-02-19 16:57:32 +0000

    aula13_14

---
+++ jscript/aula13/index.js
@@ -0,0 +1,20 @@
+/* WHILE */
+
+/* var i = 0
+
+while(i <= 10){
+    console.log(`o i vale: ${i}`)
+    i++
+} */
+
+
+
+/* DO WHILE */
+
+var i = 0
+
+do{
+    console.log(`o i vale: ${i}`)
+    i++
+}while(i <= 10)
+
---
+++ jscript/aula14/index.js
@@ -0,0 +1,10 @@
+
+
+/* LOOP FOR */
+
+var i = 0
+console.log('Vai começar ...')
+for (i=0; i<=10; i++) {
+    console.log(`Valor de i: ${i}`)
+}
+console.log('Fim ...')
